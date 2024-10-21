79db7a74dcaf05f573fc6624e491892b91f6b863
Rizzo Nicola <23456488+nrizzo@users.noreply.github.com>
2024-10-21 15:14:28 +0300

    ⚗️ vcf->MSA->iEFG experiment

---
+++ experiments/vcf-to-hapl-to-efg/README.md
@@ -0,0 +1,36 @@
+#vcf-to-hapl-to-efg experiment
+Pipeline to build an indexable Elastic Founder Graph from a VCF file plus reference. After checking out the 'Prerequisites' and 'Datasets and obtaining the input data' sections, you can build the chromosome 22 iEFG with command
+```
+/usr/bin/time ./sample-and-build-efg-heuristic.sh -f chr22_uppercase.fasta -v 1KGP.CHM13v2.0.chr22.recalibrated.snp_indel.pass.phased.vcf.gz -c chr22 -s 2504 -M 250 -t 32
+```
+This requires ~X of disk space and ~Y GB of RAM. If you want to generate an iEFG from fewer haplotypes, for example 20, run
+```
+/usr/bin/time ./sample-and-build-efg-heuristic.sh -f chr22_uppercase.fasta -v 1KGP.CHM13v2.0.chr22.recalibrated.snp_indel.pass.phased.vcf.gz -c chr22 -s 20 -M 41 -t 32
+```
+
+## Prerequisites
+The pipeline expects [`bcftools`](https://www.htslib.org/download/) and [`vcf2multialign`](https://github.com/tsnorri/vcf2multialign) to be found in the search path variable `PATH`, and expects `founderblockgraph` to be in folder `tools/founderblockgraph` from the root of this repository. In case you obtain the tools in a different way, modify lines 11-33 of `sample-and-build-efg-heuristic.sh` accordingly. To manipulate FASTQ files in the next section, we also use [`seqtk`](https://github.com/lh3/seqtk).
+
+## Datasets and obtaining the input data
+We use the [phased T2T 1KGP panel](https://zenodo.org/records/7612953) (Version 1.0) by Joseph Lalli, based on [T2T-CHM13v2.0](https://github.com/marbl/CHM13). Assuming `seqtk` is installed, we can easily obtain chromosome 22 as follows:
+```
+wget "https://s3-us-west-2.amazonaws.com/human-pangenomics/T2T/CHM13/assemblies/analysis_set/chm13v2.0.fa.gz"
+seqtk subseq chm13v2.0.fa.gz <(echo "chr22") > chr22_uppercase.fasta
+seqtk subseq ~/../scratch-hdd/nicrizzo/datasets/t2t/chm13v2.0.fa.gz <(echo "chr22") | seqtk seq -U - > chr22_uppercase.fasta
+```
+and obtain the chr22 variations as follow:
+```
+wget "https://zenodo.org/records/7612953/files/phased_T2T_panel.tar"
+tar -xvf phased_T2T_panel.tar phased_T2T_panel/1KGP.CHM13v2.0.chr22.recalibrated.snp_indel.pass.phased.vcf.gz --strip-components 1
+```
+
+## Versions of the software used
+| Tool              |          Version |
+| ----------------- | ---------------- |
+| founderblockgraph | 3f4133e (GitHub) |
+| vcf2multialign    |    1.2.2 23f3f42 |
+| seqtk             |   1.4-r130-dirty |
+| bcftools          |             1.20 |
+
+## todo
+- [] gzip some of the intermediate files
---
+++ experiments/vcf-to-hapl-to-efg/sample-and-build-efg-heuristic.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+# sample some haplotypes from reference + VCF file, build MSA with vcf2multialign, and build iEFG with founderblockgraph
+set -e
+set -o pipefail
+
+thisfolder=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd ) # https://stackoverflow.com/questions/59895/how-do-i-get-the-directory-where-a-bash-script-is-located-from-within-the-script
+
+#
+# executables
+#
+founderblockgraph=$thisfolder/../../tools/founderblockgraphs/founderblockgraph
+vcf2multialign=vcf2multialign
+bcftools=bcftools
+
+#
+# setup
+#
+threads=8
+# parsing command line options
+print_help()
+{
+   echo "usage: $0 [-f reference.fa] [-v variation.vcf] [-c chromosome] [-s nhapl] [-M Mrows] [-t threads]"
+   echo "	-h --help:  show this screen"
+   echo "	-f reference:  reference (FASTA format)                 used to generate MSA"
+   echo "	-v variation:  variation (VCF format, possibly gzipped) used to generate MSA"
+   echo "	-c chromosome: chromosome name                          used to generate MSA (see vcf2multialign)"
+   echo "	-s nhapl: sample the haplotype list and keep 'nhapl' random haplotypes"
+   echo "	-M Mrows: set parameter --heuristic-subset='Mrows' for iEFG construction (see founderblockgraph)"
+   echo "	-t threads: threads used in iEFG construction                            (see founderblockgraph)"
+}
+
+# https://stackoverflow.com/questions/12022592/how-can-i-use-long-options-with-the-bash-getopts-builtin
+for arg in "$@"; do
+	shift
+	case "$arg" in
+		'--help')                set -- "$@" '-h'   ;;
+		*)                       set -- "$@" "$arg" ;;
+	esac
+done
+
+argf=false ; argc=false ; argv=false ; args=false ; argM=false ;
+OPTIND=1
+while getopts "hf:v:c:r:s:M:t:" option; do
+	case $option in
+		f) # fasta + vcf input : fasta
+			argf=true
+			reference="$(realpath $OPTARG)" ;;
+		v) # fasta + vcf input : vcf
+			argv=true
+			vcf="$(realpath $OPTARG)" ;;
+		c) # fasta + vcf input : chromosome for vcf2multialign
+			argc=true
+			chromosome="$OPTARG" ;;
+		s) # number of samples
+			args=true
+			nhapl="$OPTARG" ;;
+		M) # heuristic subset
+			argM=true
+			heuristicsubset="$OPTARG" ;;
+		t) # threads
+			argt=true
+			threads="$OPTARG" ;;
+		h) # display help
+			print_help
+			exit;;
+		\?) # invalid option
+			echo "Error: Invalid option"
+			exit;;
+	esac
+done
+shift $(expr $OPTIND - 1) # remove options from positional parameters
+
+if [ "$argf" = false ] || [ "$argc" = false ] || [ "$argv" = false ] || [ "$args" = false ] || [ "$argM" = false ]
+then
+	print_help
+	exit
+fi
+
+outputfolder=$thisfolder/output
+mkdir $outputfolder
+if [[ $? -gt 0 ]] ; then echo "Output directory $outputfolder already exists!" ; exit 1 ; fi
+log=$outputfolder/log.txt
+stats=$outputfolder/stats.txt
+cd $outputfolder
+
+#
+# 1. sampling the vcf
+#
+echo -n "Sampling the vcf..."
+# TODO I am expecting the haplotypes to be after the 9th field specified by the VCF header, is this always correct?
+$bcftools query -l $vcf > haplotypes
+cat haplotypes | shuf -n $nhapl > sampled_haplotypes
+$bcftools view --samples-file sampled_haplotypes $vcf > sampled_haplotypes.vcf
+# FIX for the T2T 1KGP data and vcf2multialign, see https://github.com/tsnorri/vcf2multialign/issues/5
+sed -i 's/e+06//g' sampled_haplotypes.vcf
+sed -i 's/INFO=<ID=AC,Number=1/INFO=<ID=AC,Number=A/g' sampled_haplotypes.vcf
+echo " done."
+
+#
+# 2. vcf -> MSA
+#
+echo -n "Computing the MSA..."
+/usr/bin/time $vcf2multialign \
+	--founder-sequences=50 \
+	--input-reference=$reference \
+	--input-variants=sampled_haplotypes.vcf \
+	--chromosome $chromosome \
+	--output-graph variant.graph >> $log 2>> $log
+
+/usr/bin/time $vcf2multialign \
+	--input-reference=$reference \
+	--input-graph variant.graph \
+	-H \
+	-s sampled_haplotypes.a2m >> $log 2>> $log
+echo " done."
+
+#
+# 3. MSA -> iEFG
+#
+echo -n "Building the indexable Elastic Founder Graph..."
+/usr/bin/time $founderblockgraph --elastic --gfa --ignore-chars="N" --output-paths --threads=$threads --input=sampled_haplotypes.a2m --output=efg-unsimplified.gfa --heuristic-subset $heuristicsubset >> $log 2>> $log
+echo " done."
