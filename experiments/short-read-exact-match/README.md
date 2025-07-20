8f7c90af51a0332bd135ecc7f8091a4945cbe1ad
nrizzo <nrizzo@users.noreply.github.com>
2025-07-20 13:27:05 +0300

    ⚗️➕ add vg map to short read exp

--- experiments/short-read-exact-match/README.md
+++ experiments/short-read-exact-match/README.md
@@ -1,19 +1,21 @@
 # Exact match of short reads on the chr22 iEFG
-We compare the short-read exact matching solution of `efg-locate` on the chromosome 22 iEFG built with the pipeline at `experiments/vcf-to-hapl-to-efg` to that of `bwa` on the T2T-CHM13 linear reference for chromosome 22. After checking out the *Prerequisites* and *Datasets* sections, run the script `runexp.sh` (requires 42G of disk space) and check `output/runexp_log.txt` for the results.
+We compare the short-read exact matching solution of `efg-locate` on the chromosome 22 iEFG built with the pipeline at `experiments/vcf-to-hapl-to-efg` to that of: `bwa`, on the T2T-CHM13 linear reference for chromosome 22; and `vg map`, on the (pruned) chromosome 22 graph built from the same VCF as the iEFG. After checking out the *Prerequisites* and *Datasets* sections, run the script `runexp.sh` (requires ~150G of disk space for the results) and check `output/runexp_log.txt` for the results.
 
 ## Prerequisites
-Script `runexp.sh` expects `efg-locate`, `bwa`, and `seqtk` to be located in folder `tools/efg-locate`, `tools/bwa`, and `tools/seqtk` from the root of this repository. You can download and compile them with the following commands (executed from this folder):
+Script `runexp.sh` expects `efg-locate`, `bwa`, `seqtk`, and `vg` to be located in folders `tools/efg-locate`, `tools/bwa`, `tools/seqtk`, and `tools/vg/bin` from the root of this repository. You can download and compile them with the following commands (executed from this folder):
 ```console
 make -C ../../tools/efg-locate
 git submodule update --init ../../tools/{bwa,seqtk}
+mkdir --parents ../../tools/vg/bin && wget https://github.com/vgteam/vg/releases/download/v1.67.0/vg --output-document=../../tools/vg/bin
 make -C ../../tools/bwa
 make -C ../../tools/seqtk
 ```
 
 ## Datasets
-Obtain the graph (262M) with command
+Obtain the graphs (544M) with command
 ```console
-wget https://zenodo.org/records/15112649/files/chr22_iEFG.gfa.gz?download=1 --output-document=input/chr22_iEFG.gfa.gz && gunzip input/chr22_iEFG.gfa.gz
+wget https://zenodo.org/records/15189487/files/chr22_iEFG.gfa.gz?download=1 --output-document=input/chr22_iEFG.gfa.gz && gunzip input/chr22_iEFG.gfa.gz
+wget https://zenodo.org/records/15189487/files/chr22_vg.gfa.gz?download=1 --output-document=input/chr22_vg.gfa.gz && gunzip input/chr22_vg.gfa.gz
 ```
 the reads (24GB) with
 ```console
--- experiments/short-read-exact-match/runexp.sh
+++ experiments/short-read-exact-match/runexp.sh
@@ -8,13 +8,16 @@ cd $thisfolder
 bwa=$thisfolder/../../tools/bwa/bwa
 efglocate=$thisfolder/../../tools/efg-locate/efg-locate
 seqtk=$thisfolder/../../tools/seqtk/seqtk
+vg=$thisfolder/../../tools/vg/bin/vg
 usrbintime=/usr/bin/time
 
 # params
 inputgraph=$thisfolder/input/chr22_iEFG.gfa
+inputvggraph=$thisfolder/input/chr22_vg.gfa
 inputreference=$thisfolder/input/chr22_uppercase.fasta
 inputreads=$thisfolder/input/ERR1025645_sample05_1.fq.gz
-threads=16
+map_threads=16
+index_threads=64
 
 # 0. setup
 mkdir output
@@ -23,7 +26,7 @@ echo -n > output/runexp_log.txt
 echo "# 1. match in the chr22 iEFG (efg-locate)" >> output/runexp_log.txt
 /usr/bin/time $efglocate \
 	--reverse-complement \
-	--threads $threads \
+	--threads $map_threads \
 	$inputgraph \
 	<(seqtk seq -A $inputreads) \
 	output/efg_locate_matches.gaf \
@@ -32,7 +35,7 @@ echo "# 1. match in the chr22 iEFG (efg-locate)" >> output/runexp_log.txt
 echo "# 2. match in the chr22 reference (bwa)" >> output/runexp_log.txt
 ln -s $inputreference $thisfolder/output/ref.fasta
 /usr/bin/time $bwa index output/ref.fasta 2>> output/runexp_log.txt
-/usr/bin/time $bwa aln -n 0 -k 0 -l 100 -t $threads \
+/usr/bin/time $bwa aln -n 0 -k 0 -l 100 -t $map_threads \
 	output/ref.fasta \
 	$inputreads \
 	> output/bwa_matches.sai 2>> output/runexp_log.txt
@@ -42,9 +45,32 @@ ln -s $inputreference $thisfolder/output/ref.fasta
 	$inputreads \
 	> output/bwa_matches.sam 2>> output/runexp_log.txt
 
-echo "# 3. compute stats" >> output/runexp_log.txt
+echo "# 3. index and match in the chr22 vg (vg map)" >> output/runexp_log.txt
+ln -s $inputvggraph output/graph.gfa
+/usr/bin/time $vg autoindex \
+        --verbosity 1 \
+        --threads $index_threads \
+        --prefix output/index \
+        --workflow map \
+        --gfa output/graph.gfa \
+        --tmp-dir output \
+        2>> output/runexp_log.txt
+/usr/bin/time $vg map \
+        --base-name output/index \
+        --fastq $inputreads \
+        --threads $map_threads \
+        --min-mem 100 \
+        --hit-max 0 \
+        --try-up-to 1 \
+        --gaf > output/vg_matches.gaf \
+        2>> output/runexp_log.txt
+
+echo "# 4. compute stats" >> output/runexp_log.txt
 echo -n "efg-locate took" $(grep system output/runexp_log.txt | head -n 1 | cut -d' ' -f3 | cut -d'e' -f1) >> output/runexp_log.txt
 echo " and matched" $(cut -f1 output/efg_locate_matches.gaf | uniq | sort | uniq | wc -l) "reads" >> output/runexp_log.txt
 
-echo -n "bwa took" $(grep system output/runexp_log.txt | tail -n 3 | cut -d' ' -f3 | cut -d'e' -f1 | tr "\n" " ") >> output/runexp_log.txt
+echo -n "bwa took" $(grep system output/runexp_log.txt | tail -n +2 | head -n 3 | cut -d' ' -f3 | cut -d'e' -f1 | tr "\n" " ") >> output/runexp_log.txt
 echo " and matched" $(cat output/bwa_matches.sam | awk '{if ($6 == "100M") {print}}' | cut -f1 | uniq | sort | uniq | wc -l) "reads" >> output/runexp_log.txt
+
+echo -n "vg took" $(grep system output/runexp_log.txt | tail -n +5 | head -n 2 | cut -d' ' -f3 | cut -d'e' -f1 | tr "\n" " ") >> output/runexp_log.txt
+echo " and matched" $(cat output/vg_matches.gaf | awk '{if ($3 != "*") {print}}' | cut -f1 | uniq | sort | uniq | wc -l) "reads" >> output/runexp_log.txt
